808d5386d9e07de68ee579e695a76974b10eec05
Garth Lilly <garff247@gmail.com>
2019-04-28 00:58:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,22 +4,18 @@ class Dog
   attr_accessor :name
 
 @@all = []
-@@names = []
   def initialize(name)
     @name = name
     @@all << self
-    @@names << name
-    puts @@names
   end
 
   def self.clear_all
     @@all.clear
-    @@names.clear
   end
 
   def self.all
-    @@names.each do |name|
-      puts name
+    @@all.each do |dog|
+      puts dog.name
     end
   end
 end
